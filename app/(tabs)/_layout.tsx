1e4892c6eacf7b7906740875700dcab3209c56a5
kirito <kirito666codr@gmail.com>
2026-01-25 23:39:03 +0530

    feat: implement custom TabIcon component and enhance tab navigation layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,32 +1,83 @@
+import TabIcon from "@/components/TabIcon";
+import { icons } from "@/constants/icons";
 import { Tabs } from "expo-router";
 import React from "react";
 
 export default function _layout() {
   return (
-   <Tabs>
+   <Tabs 
+   screenOptions={{
+    tabBarShowLabel:false,
+    tabBarItemStyle:{
+        width:'100%',
+        height:'100%',
+        justifyContent:"center",
+        alignItems:"center",
+    },
+    tabBarStyle:{
+        backgroundColor:"#0f0D23",
+        borderRadius: 50,
+        marginHorizontal: 20,
+        marginBottom: 36,
+        height:52,
+        position:"absolute",
+        overflow:"hidden",
+        borderWidth:1,
+        borderColor:"#0f0D23"
+    }
+   }}
+   >
     <Tabs.Screen
        name="index"
        options={{
         title:'Home',
-        headerShown:false
+        headerShown:false,
+        tabBarIcon: ({focused})=>(
+            <TabIcon 
+            focused={focused}
+            icon={icons.home}
+            title="Home"
+            />
+        )
        }}/>
     <Tabs.Screen
        name="search"
        options={{
         title:'Search',
-        headerShown:false
+        headerShown:false,
+          tabBarIcon: ({focused})=>(
+            <TabIcon 
+            focused={focused}
+            icon={icons.search}
+            title="Search"
+            />
+        )
        }}/>
     <Tabs.Screen
        name="saved"
        options={{
         title:'Saved',
-        headerShown:false
+        headerShown:false,
+          tabBarIcon: ({focused})=>(
+            <TabIcon 
+            focused={focused}
+            icon={icons.save}
+            title="Saved"
+            />
+        )
        }}/>
     <Tabs.Screen
        name="profile"
        options={{
         title:'Profile',
-        headerShown:false
+        headerShown:false,
+          tabBarIcon: ({focused})=>(
+            <TabIcon 
+            focused={focused}
+            icon={icons.person}
+            title="Profile"
+            />
+        )
        }}/>
    </Tabs>
   );
---
+++ components/TabIcon.tsx
@@ -0,0 +1,33 @@
+import { images } from "@/constants/images";
+import React from "react";
+import { Image, ImageBackground, ImageSourcePropType, Text, View } from "react-native";
+
+type TabIconProps = {
+    focused:boolean
+    icon:ImageSourcePropType
+    title:string
+}
+
+const TabIcon = ({focused,icon,title}:TabIconProps) => {
+    if(focused){
+        return (
+            <>
+               <ImageBackground
+               source={images.highlight}
+               className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+               >
+                   <Image source={icon} tintColor="#151312" className="size-5"/>
+                   <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
+               </ImageBackground>
+               </>
+  );
+}
+
+return(
+    <View className="size-full justify-center items-center rounded-full">
+<Image source={icon} tintColor="#A8B5DB" className="size-5"/>
+    </View>
+)
+};
+
+export default TabIcon;
