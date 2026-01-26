7c9461d314cc32a5b402c6fcdd5b9d746c18c1a9
kirito <kirito666codr@gmail.com>
2026-01-26 21:56:49 +0530

    feat: implement Searchbar component with customizable placeholder and onPress functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
+import Searchbar from "@/components/Searchbar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
-import { SearchBar } from "react-native-screens";
 
 export default function Index() {
+  const router = useRouter();
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg}  className="absolute w-full z-0" />
@@ -15,7 +17,10 @@ export default function Index() {
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
         <View className="flex-1 mt-5">
-          <SearchBar />
+          <Searchbar 
+          onPress={()=> router.push("/search")}
+          placeholder="Search for a movie"
+          />
         </View>
       </ScrollView>
     </View>
--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,11 +1,23 @@
 import { icons } from "@/constants/icons";
 import React from "react";
-import { Image, View } from "react-native";
+import { Image, TextInput, View } from "react-native";
 
-const Searchbar = () => {
+type props ={
+    placeholder:string
+    onPress?:()=> void
+}
+
+const Searchbar = ({onPress,placeholder}:props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image source={icons.search} className="size-5" resizeMode="center" tintColor="#ab8bff"/>
+      <TextInput
+      onPress={onPress}
+      placeholder={placeholder}
+      value=""
+      onChange={()=>{}}
+      placeholderTextColor={"#a8b5db"}
+      />
     </View>
   );
 };
